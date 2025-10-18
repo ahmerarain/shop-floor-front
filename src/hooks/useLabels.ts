1f89893a7e46b268610831763e96a1ee5e6e7345
Ahmer Arain <ahmer@gordiansol.com>
2025-10-18 19:03:23 +0500

    fixed issue

--- src/hooks/useLabels.ts
+++ src/hooks/useLabels.ts
@@ -1,10 +1,11 @@
 import { useMutation } from "@tanstack/react-query";
-import { csvApi, BulkLabelResponse } from "../services/api";
+import { labelApi } from "../services/label/label.service";
+import { BulkLabelResponse } from "../services/label";
 
 // Hook for generating ZPL label
 export const useGenerateZPLLabel = () => {
   return useMutation({
-    mutationFn: (id: number) => csvApi.generateZPLLabel(id),
+    mutationFn: (id: number) => labelApi.generateZPLLabel(id),
     onSuccess: (response, id) => {
       // Create blob and trigger download
       const blob = new Blob([response.data], { type: "text/plain" });
@@ -23,7 +24,7 @@ export const useGenerateZPLLabel = () => {
 // Hook for generating PDF label
 export const useGeneratePDFLabel = () => {
   return useMutation({
-    mutationFn: (id: number) => csvApi.generatePDFLabel(id),
+    mutationFn: (id: number) => labelApi.generatePDFLabel(id),
     onSuccess: (response, id) => {
       // Create blob and trigger download
       const blob = new Blob([response.data], { type: "application/pdf" });
@@ -42,7 +43,7 @@ export const useGeneratePDFLabel = () => {
 // Hook for generating bulk labels
 export const useGenerateBulkLabels = () => {
   return useMutation({
-    mutationFn: (ids: number[]) => csvApi.generateBulkLabels(ids),
+    mutationFn: (ids: number[]) => labelApi.generateBulkLabels(ids),
     onSuccess: (response: { data: BulkLabelResponse }) => {
       // Handle bulk label generation response
       console.log("Bulk labels generated:", response.data);
@@ -53,7 +54,7 @@ export const useGenerateBulkLabels = () => {
 // Hook for downloading ZPL file
 export const useDownloadZPLFile = () => {
   return useMutation({
-    mutationFn: (id: number) => csvApi.downloadZPLFile(id),
+    mutationFn: (id: number) => labelApi.downloadZPLFile(id),
     onSuccess: (response, id) => {
       const blob = new Blob([response.data], { type: "text/plain" });
       const url = window.URL.createObjectURL(blob);
@@ -71,7 +72,7 @@ export const useDownloadZPLFile = () => {
 // Hook for downloading PDF file
 export const useDownloadPDFFile = () => {
   return useMutation({
-    mutationFn: (id: number) => csvApi.downloadPDFFile(id),
+    mutationFn: (id: number) => labelApi.downloadPDFFile(id),
     onSuccess: (response, id) => {
       const blob = new Blob([response.data], { type: "application/pdf" });
       const url = window.URL.createObjectURL(blob);
